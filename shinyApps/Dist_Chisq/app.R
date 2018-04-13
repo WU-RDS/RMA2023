e8c5992dec6e6167f304658a05d2dfa4fa4da1ef
Daniel Winkler <winkler.daniel1@gmail.com>
2018-04-13 16:13:24 +0200

    pretty much finished chisquared

--- shinyApps/Dist_Chisq/app.R
+++ shinyApps/Dist_Chisq/app.R
@@ -1,14 +1,17 @@
 library(shiny)
 library(ggplot2)
-
+library(xtable)
 ui <- fluidPage(
+    withMathJax(),
   sidebarLayout(
     sidebarPanel(sliderInput("df", 
                              "Degrees of freedom", 
                              min = 1,
                              max = 30,
                              value = 4, 
-                             step = 1)
+                             step = 1),
+                 tableOutput("percentiles"),
+                 uiOutput("moments")
     ),
     mainPanel(
       plotOutput("distplot", height = "260"),
@@ -21,6 +24,21 @@ ui <- fluidPage(
 
 
 server <- function(input, output) {
+
+    output$percentiles <- renderTable({
+    pcts <- round(qchisq(p = c(0.05, 0.25, 0.5, 0.75, 0.95), df = input$df), digits = 3)
+    pctnames <- c("5th", "25th", "50th", "75th", "95th")
+    pcttab <- data.frame(pctnames, pcts)
+    colnames(pcttab) <- c("Percentile", "Value")
+    xtable(pcttab)
+  }, caption = "Values of the selected Chi-Squared distribution at different percentiles")
+
+  output$moments <- renderUI({
+    EQ <- input$df
+    VARQ <- 2 * input$df
+    out <- paste0("$$Q\\sim \\chi^2(", input$df, ") \\\\ \\mathbb{E}[Q]=", EQ, ", Var(Q)=", VARQ, "$$")
+    withMathJax(helpText(out))
+  })
   
   output$distplot <- renderPlot({
     
