12b7f082641e571f61753eb447b545e3f0e92af8
Knaus <pknaus@ad.wu-wien.ac.at>
2018-04-18 14:42:29 +0200

    Finished first draft of theoretical part of advanced data handling. Began work on case study for advanced data handling

--- Beginning of worked example.R
+++ Beginning of worked example.R
@@ -1,4 +1,4 @@
-data <-readr::read_csv("./MRDA2018/MRD_A3_Survey.csv")
+data <-read.csv("./MRDA2018/MRD_A3_Survey.csv", stringsAsFactors = FALSE)
 
 data <- data[-c(1,2),]
 
@@ -7,20 +7,24 @@ library(stringr)
 
 bla <- data %>%
   filter(Progress == 100) %>%
-  mutate_at(paste0("Q63_", 1:8), as.numeric) %>%
-  mutate_at(paste0("Q18_1_", 1:8), str_remove_all, pattern = "€") %>%
+  mutate_at(vars(starts_with("Q63")), as.numeric) 
+
+%>%
+  mutate_at(vars(starts_with("Q63")), str_remove_all, pattern = "€") %>%
   mutate_at(paste0("Q18_1_", 1:8), str_replace_all, pattern = ",", replacement = ".") %>%
   select(paste0("Q18_1_", 1:8))
 
 
+price_cols <- paste0("Q18_1_", 1:8)
+numeric_trans_cols <- c(paste0("Q63_", 1:8), price_cols)
+
 data %>%
   filter(Progress == 100) %>%
-  mutate_at(paste0("Q63_", 1:8), as.numeric) %>%
-  mutate_at(paste0("Q18_1_", 1:8), str_remove_all, pattern = "[^0-9,\\.]") %>%
-  mutate_at(paste0("Q18_1_", 1:8), str_replace_all, pattern = ",", replacement = ".") %>%
-  mutate_at(paste0("Q18_1_", 1:8), as.numeric) %>%
-  select(paste0("Q18_1_", 1:8)) %>%
-  View()
+  mutate_at(price_cols, str_remove_all, pattern = "[^0-9,\\.]") %>%
+  mutate_at(price_cols, str_replace_all, pattern = ",", replacement = ".") %>%
+  mutate_at(numeric_trans_cols, as.numeric) 
+
+str_split()
 
 
-apply(is.na(bla) == is.na(bla2), 2, function(x) (which(x == FALSE)))
+
