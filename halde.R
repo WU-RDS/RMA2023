acc8e429a5ff19c276520b49773e750202e4e7f9
WUIMSM <nils.wloemert@wu.ac.at>
2020-09-28 01:23:22 +0200

    restructure-9

---
+++ halde.R
@@ -0,0 +1,19 @@
+install.packages('Lahman')
+
+::: {.lc_green}
+:::
+  
+::: {.lc_green}
+Learning check
+:::
+  
+  x <- knitr::kable(head(mtcars), "html")
+column_spec(x, 1:2, width = "20em", bold = TRUE, italic = TRUE)
+
+
+system('cal 2020')
+
+
+::: {.infobox_orange .hint data-latex="{hint}"}
+One popular R package for predictive modeling using machine learning is the [XGBoost Package](https://xgboost.readthedocs.io/en/latest/R-package/xgboostPresentation.html). We will cover predictive models later, but feel free to check out this package already if you are interested.  
+:::
