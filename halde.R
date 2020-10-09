f5e2f3141027059f1e8100163e88b0599dcf2bab
WUIMSM <nils.wloemert@wu.ac.at>
2020-10-09 10:32:08 +0200

    restructure-16

--- halde.R
+++ halde.R
@@ -17,3 +17,14 @@ system('cal 2020')
 ::: {.infobox_orange .hint data-latex="{hint}"}
 One popular R package for predictive modeling using machine learning is the [XGBoost Package](https://xgboost.readthedocs.io/en/latest/R-package/xgboostPresentation.html). We will cover predictive models later, but feel free to check out this package already if you are interested.  
 :::
+  
+  
+library(COVID19)
+gmr <- "https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv"
+covid_data <- covid19(
+  country = "US",
+  level = 2,
+  start = "2020-01-01",
+  gmr = gmr
+)
+head(covid_data)
